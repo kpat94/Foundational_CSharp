8884f5067fba9e3593a1adb4198347eee336c1b9
kpat94 <pkkinage@gmail.com>
2025-10-28 16:55:02 +1030

    Arrays

---
+++ C#_Arrays/4_Challenge/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] fraudulentOrderIds = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string item in fraudulentOrderIds)
+{
+    if(item.StartsWith('B'))
+    {
+        Console.WriteLine(item);    
+    }
+}
